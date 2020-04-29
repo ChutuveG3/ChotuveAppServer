4e2555c4a8a4502ea897993bd04d6e0e131270da
Santiago Almejun <santi.alme@gmail.com>
2020-04-29 10:46:55 -0300

    small notes

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,8 +1,9 @@
 const axios = require('axios').default;
 
 exports.users = (req, res) => {
+  // Validar req.body params
   axios
-    .post('http://localhost:8081/users', req)
+    .post('http://localhost:8081/users', req.body)
     .then(response => {
       // eslint-disable-next-line no-console
       console.log(response.data.url);
