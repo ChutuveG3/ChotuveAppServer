50f308672fd927fc996ef075970c14cfc613f9b0
bauticanavese <bauti-canavese@hotmail.com>
2020-05-05 18:23:19 -0300

    fix-add response message on success

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,6 +3,9 @@ const { signupUser } = require('../services/users');
 exports.signup = ({ body }, res, next) =>
   signupUser(body)
     .then(() => {
-      res.status(201).end();
+      res
+        .status(201)
+        .json({ message: 'ok' })
+        .end();
     })
     .catch(err => next(err));
