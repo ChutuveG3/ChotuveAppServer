dd91c1ce9b8c137f0cdf451bbc62de9e03b02c60
Santiago Almejun <santi.alme@gmail.com>
2020-05-16 14:27:38 -0300

    removed unused code

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,15 +1,3 @@
-const { Schema, model } = require('mongoose');
-
 exports.home = (req, res) => {
-  // eslint-disable-next-line no-unused-vars
-  const a = new Schema({
-    test: String,
-    funca: Boolean
-  });
-  // eslint-disable-next-line no-unused-vars
-  const ab = model('ab', a);
-  // eslint-disable-next-line new-cap
-  const prueba = new ab({ test: 'testing', funca: true });
-  prueba.save();
   res.send({ message: 'App server hello world!' });
 };
