cffdb2dfb13a39e38db650444ed6858b56024341
Santiago Almejun <santi.alme@gmail.com>
2020-05-17 11:01:10 -0300

    minor fix

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,10 +3,7 @@ const { signupUser, loginUser } = require('../services/users');
 exports.signup = ({ body }, res, next) =>
   signupUser(body)
     .then(() => {
-      res
-        .status(201)
-        .json({ message: 'ok' })
-        .end();
+      res.status(201).send({ message: 'ok' });
     })
     .catch(err => next(err));
 
