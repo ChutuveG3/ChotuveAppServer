0b97730eab9feda3110b813ad27827096ea39819
Santiago Almejun <santi.alme@gmail.com>
2020-05-30 16:03:28 -0300

    explicitly marked authorization header field as not optional

--- app/middlewares/users.js
+++ app/middlewares/users.js
@@ -62,6 +62,7 @@ exports.getCurrentUserSchema = {
   authorization: {
     in: ['headers'],
     isString: true,
+    optional: false,
     errorMessage: 'authorization should be a string and be present in headers'
   }
 };
--- app/middlewares/videos.js
+++ app/middlewares/videos.js
@@ -4,6 +4,7 @@ exports.createVideoSchema = {
   authorization: {
     in: ['headers'],
     isString: true,
+    optional: false,
     errorMessage: 'authorization should be a string and be present in headers'
   },
   title: {
