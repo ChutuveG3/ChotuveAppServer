3797db83c340b225afca383b6d571c25aabbcb2b
Lucas <lnmedrano@fi.uba.ar>
2020-06-18 18:04:08 -0300

    change push wording

--- app/utils/push_builder.js
+++ app/utils/push_builder.js
@@ -1,17 +1,17 @@
 exports.sendFriendRequestPushBuilder = ({ srcUsername, dstUserFirebaseToken }) => ({
-  title: 'Nueva solicitud de amistad.',
-  body: `${srcUsername} te envió una solicitud de amistad`,
+  title: 'New friend request!',
+  body: `${srcUsername} has sent you a friend request.`,
   firebaseToken: dstUserFirebaseToken
 });
 
 exports.acceptFriendRequestPushBuilder = ({ srcUsername, dstUserFirebaseToken }) => ({
-  title: 'Tenés un nuevo amigo.',
-  body: `${srcUsername} aceptó tu solicitud de amistad`,
+  title: 'You have a new friend!',
+  body: `${srcUsername} has accepted your friend request.`,
   firebaseToken: dstUserFirebaseToken
 });
 
 exports.newVideoPushBuilder = ({ username, friendFirebaseToken }) => ({
-  title: 'Hay un nuevo video esperándote.',
-  body: `${username} subió un nuevo video. No te lo pierdas! `,
+  title: 'There is a new video waiting for you.',
+  body: `${username} has uploaded a new video. Check it out! `,
   firebaseToken: friendFirebaseToken
 });
