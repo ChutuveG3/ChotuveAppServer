df884b67e298ede418264a0d2f527b29c4efa839
Santiago Almejun <santi.alme@gmail.com>
2020-06-30 10:19:26 -0300

    truncate database after all tests

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,12 @@
+const { truncateUserCollection, truncateVideoCollection } = require('./utils/utils');
+
 /* eslint-disable global-require */
 jest.setTimeout(5000);
 beforeAll(async () => {
   // eslint-disable-next-line no-unused-vars
   const db = await require('../config/db');
+});
+afterAll(async () => {
+  await truncateUserCollection();
+  await truncateVideoCollection();
 });
