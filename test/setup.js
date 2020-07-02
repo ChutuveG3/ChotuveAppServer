3e90be555fb44ed1507e09706d7c6cbc51d2572e
Santiago Almejun <santi.alme@gmail.com>
2020-07-02 10:17:38 -0300

    added cors and truncate database before tests

--- test/setup.js
+++ test/setup.js
@@ -5,6 +5,8 @@ jest.setTimeout(5000);
 beforeAll(async () => {
   // eslint-disable-next-line no-unused-vars
   const db = await require('../config/db');
+  await truncateUserCollection();
+  await truncateVideoCollection();
 });
 afterAll(async () => {
   await truncateUserCollection();
