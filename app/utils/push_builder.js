c74280c770147b31394a0295afbe95e48208a6e8
Santiago Almejun <santi.alme@gmail.com>
2020-07-27 15:03:24 -0300

    trying to push

--- app/utils/push_builder.js
+++ app/utils/push_builder.js
@@ -17,7 +17,7 @@ exports.newVideoPushBuilder = ({ username, friendFirebaseToken }) => ({
 });
 
 exports.deleteVideoPushBuilder = ({ ownerFirebaseToken }) => ({
-  title: 'Video deleted.',
+  title: 'Video deleted',
   body: 'One of your videos has been taken down.',
   firebaseToken: ownerFirebaseToken
 });
